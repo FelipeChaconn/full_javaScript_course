f572ebd672d745f18d9d4dd2adb1283e7df459b4
felipechaconn <lf.chacon07@gmail.com>
2020-10-21 18:29:26 -0600

    Hello World

---
+++ HelloWorld/js/helloWorld.js
@@ -0,0 +1,7 @@
+/*
+Comentario multilinea
+*/
+
+//Comentario de una linea
+document.write("Hello World desde otro archivo");
+console.log("Hello World Console");
---
+++ HelloWorld/js/variables.js
@@ -0,0 +1,8 @@
+//Con el use strict lo que hacemos es crear el codigo respete todo lo que es JS
+'use strict'
+
+//Contenedor de informacion
+let pais= "Costa Rica";
+
+//Contenedor que no puede cambiar el valor
+const continente = "America"
